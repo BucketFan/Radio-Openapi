fbdbdd954b814a9934b8d5c197b773bef73c62c4
Yasushi Sakita <yasushi.sakita@gmail.com>
2022-11-14 16:04:49 +0900

    $npm run build:packages による、自動生成コードをコミット

--- packages/api_client_ts/apis/index.ts
+++ packages/api_client_ts/apis/index.ts
@@ -1,3 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './ChapterApi';
 export * from './DefaultApi';
+export * from './ProgramApi';
+export * from './ProgramsApi';
+export * from './ReactionCommentsApi';
---
+++ packages/api_client_ts/models/index.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Chapter';
+export * from './ChapterOfPlayLogEndChapterId200Response';
+export * from './ChapterOfPlayLogEndChapterIdRequest';
+export * from './ChapterOfPlayLogStartChapterIdRequest';
+export * from './Club';
+export * from './CreateProgram200Response';
+export * from './CreateProgramRequest';
+export * from './CreateProgramRequestChaptersInner';
+export * from './DeleteChapterId200Response';
+export * from './GetProgramChapters200Response';
+export * from './GetProgramReactionComments200Response';
+export * from './GetPrograms200Response';
+export * from './Plan';
+export * from './PostProgramReactionComments200Response';
+export * from './PostProgramReactionCommentsRequest';
+export * from './PostPublishPreSignedUrl200Response';
+export * from './PostPublishPreSignedUrlRequest';
+export * from './Profile';
+export * from './Program';
+export * from './ReactionComment';
